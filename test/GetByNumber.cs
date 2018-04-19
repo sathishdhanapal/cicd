c4650f2bed1ce38f6b24daf4ce3f149bd1489497
Sathish Dhanapal (Britehouse) <Sathish.Dhanapal@britehouse.co.za>
2018-04-19 12:08:46 +0200

    added httpget test

---
+++ test/GetByNumber.cs
@@ -0,0 +1,32 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using api.Dto;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace test
+{
+
+    public class GetByNumber : ApiControllerTestBase
+    {
+        private readonly HttpClient _client;
+
+        public GetByNumber()
+        {
+            _client = base.GetClient();
+        }
+
+        [Theory]
+        [InlineData("numbers")]
+        public async Task ReturnsTextForTheNumber43(string controllerName)
+        {
+            var response = await _client.GetAsync($"/api/{controllerName}/43");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<NumberResponse>(stringResponse);
+
+            Assert.Equal(43, result.Number);
+            Assert.NotEmpty(result.Text);
+        }
+    }
+}
