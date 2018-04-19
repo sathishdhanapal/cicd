905fbe49f9e22861ebb65a627b4183f7459e0d11
Sathish Dhanapal (Britehouse) <Sathish.Dhanapal@britehouse.co.za>
2018-04-19 11:37:45 +0200

    added sample number api

---
+++ api/Controllers/NumbersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using api.Dto;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class NumbersController : Controller
+    {
+        [HttpGet("{number}")]
+        public async Task<NumberResponse> GetNumberAsync(int number)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://numbersapi.com/" + number + "?json");
+            var result = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<NumberResponse>(result);
+        }
+    }
+}
